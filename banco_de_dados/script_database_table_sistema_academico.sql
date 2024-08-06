30675c3d06684d269c53208bcb63849d65226361
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-08-05 23:12:59 -0300

    init

---
+++ banco_de_dados/script_database_table_sistema_academico.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE sistema_academico;
+
+CREATE TABLE Alunos (
+    id_aluno INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    matricula VARCHAR(10) NOT NULL UNIQUE,
+    data_matricula TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id_curso_fk INT NOT NULL,
+    FOREIGN KEY (id_curso_fk) REFERENCES Cursos (id_curso)
+);
+
+CREATE TABLE Cursos (
+    id_curso INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    codigo VARCHAR(10) NOT NULL UNIQUE,
+    nome VARCHAR(100) NOT NULL,
+    carga_horaria FLOAT NOT NULL
+ );
+ 
