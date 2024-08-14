6d8ff9d54028c9a2c8d9212f8cb2a7407af961f8
FranciscoPE1984 <123676214+FranciscoPE1984@users.noreply.github.com>
2024-08-14 20:53:43 -0300

    Update aluno.py
    -Criada Classe Aluno
    -Código considerando uma pasta config e uma database
    -Caso for ficar só com arquivo config vou fazer os ajustes necessários
    -Nos métodos de Buscar, Atualizar e Deletar realizei a pesquisa tanto pelo id_aluno quanto pela matricula. Caso vcs queiram só pelo id_aluno posso ajustar o código.
    -Criei o Banco de Dados na minha máquina, fiz a conexão e todos os métodos funcionaram ok.

--- programa_sistema_academico/aluno.py
+++ programa_sistema_academico/aluno.py
@@ -1,27 +1,72 @@
-from config import Config
+#código considerando uma pasta config e uma databese
+#Caso ficar só com o arquivo config vou fazer os ajustes necessários
+
+from database.database import Database
 
 class Aluno:
+    def __init__(self, nome=None, data_nascimento=None, matricula=None, id_curso_fk=None, id_aluno=None):
+        self.__id_aluno = id_aluno
+        self.__nome = nome
+        self.__data_nascimento = data_nascimento
+        self.__matricula = matricula
+        self.__id_curso_fk = id_curso_fk
 
-    print()  ## excluir o print
+    def adicionar_aluno(self):
+        db = Database()
+        query = '''INSERT INTO Alunos (nome, data_nascimento, matricula, id_curso_fk) 
+                   VALUES (%s, %s, %s, %s)'''
+        db.cursor.execute(query, (self.__nome, self.__data_nascimento, self.__matricula, self.__id_curso_fk))
+        db.conexao.commit()
+        db.cursor.close()
+        db.conexao.close()
+        return True
 
+    @staticmethod
+    def listar_alunos():
+        db = Database()
+        query = '''SELECT * FROM Alunos'''
+        db.cursor.execute(query)
+        alunos = db.cursor.fetchall()
+        db.cursor.close()
+        db.conexao.close()
+        return alunos
 
-    ## construtor --> os atributos são privados
-    # def __init__():
+    def buscar_aluno(self, identificador):
+        db = Database()
+        if isinstance(identificador, int):
+            query = '''SELECT * FROM Alunos WHERE id_aluno = %s'''
+            db.cursor.execute(query, (identificador,))
+        else:
+            query = '''SELECT * FROM Alunos WHERE matricula = %s'''
+            db.cursor.execute(query, (identificador,))
+        aluno = db.cursor.fetchone()
+        db.cursor.close()
+        db.conexao.close()
+        return aluno
 
-    ## metodo privado para conectar com banco de dados
-    # def __conectar(self):
+    def atualizar_aluno(self, novo_nome=None, nova_data_nascimento=None, nova_matricula=None, novo_id_curso_fk=None):
+        db = Database()
+        query = '''UPDATE Alunos 
+                   SET nome = %s, data_nascimento = %s, matricula = %s, id_curso_fk = %s 
+                   WHERE id_aluno = %s OR matricula = %s'''
+        db.cursor.execute(query, (
+            novo_nome or self.__nome, 
+            nova_data_nascimento or self.__data_nascimento, 
+            nova_matricula or self.__matricula, 
+            novo_id_curso_fk or self.__id_curso_fk,
+            self.__id_aluno,
+            self.__matricula
+        ))
+        db.conexao.commit()
+        db.cursor.close()
+        db.conexao.close()
+        return True
 
-    ## metodo para adicionar aluno
-    # def adicionar_aluno(self):
-
-    ## metodo para listar todos os alunos
-    # def listar_alunos():
-
-    ## metodo para buscar o aluno pela matricula do aluno
-    ## buscar_aluno(self):
-
-    ## metodo para atualizar dados do aluno
-    ## def atualizar_aluno(self):
-
-    ## metodo para deletar o aluno
-    ## def deletar_aluno(self):
+    def deletar_aluno(self):
+        db = Database()
+        query = '''DELETE FROM Alunos WHERE id_aluno = %s OR matricula = %s'''
+        db.cursor.execute(query, (self.__id_aluno, self.__matricula))
+        db.conexao.commit()
+        db.cursor.close()
+        db.conexao.close()
+        return True
