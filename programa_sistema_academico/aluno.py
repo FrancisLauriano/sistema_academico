7c728ecfe867192d4b067840052b051d156fd1d2
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-08-16 10:12:47 -0300

    ok

--- programa_sistema_academico/aluno.py
+++ programa_sistema_academico/aluno.py
@@ -1,7 +1,7 @@
 #código considerando uma pasta config e uma databese
 #Caso ficar só com o arquivo config vou fazer os ajustes necessários
 
-from database.database import Database
+from database import Database
 
 class Aluno:
     def __init__(self, nome=None, data_nascimento=None, matricula=None, id_curso_fk=None, id_aluno=None):
@@ -13,60 +13,62 @@ class Aluno:
 
     def adicionar_aluno(self):
         db = Database()
+        db.conectar()
         query = '''INSERT INTO Alunos (nome, data_nascimento, matricula, id_curso_fk) 
                    VALUES (%s, %s, %s, %s)'''
-        db.cursor.execute(query, (self.__nome, self.__data_nascimento, self.__matricula, self.__id_curso_fk))
-        db.conexao.commit()
-        db.cursor.close()
-        db.conexao.close()
+        db.get_cursor().execute(query, (self.__nome, self.__data_nascimento, self.__matricula, self.__id_curso_fk))
+        db.get_conexao().commit()
+        db.get_cursor().close()
+        db.get_conexao().close()
         return True
 
     @staticmethod
     def listar_alunos():
         db = Database()
+        db.conectar()
         query = '''SELECT * FROM Alunos'''
-        db.cursor.execute(query)
-        alunos = db.cursor.fetchall()
-        db.cursor.close()
-        db.conexao.close()
+        db.get_cursor().execute(query)
+        alunos = db.get_cursor().fetchall()
+        db.get_cursor().close()
+        db.get_conexao().close()
         return alunos
 
-    def buscar_aluno(self, identificador):
+    def buscar_aluno(self):
         db = Database()
-        if isinstance(identificador, int):
-            query = '''SELECT * FROM Alunos WHERE id_aluno = %s'''
-            db.cursor.execute(query, (identificador,))
-        else:
-            query = '''SELECT * FROM Alunos WHERE matricula = %s'''
-            db.cursor.execute(query, (identificador,))
-        aluno = db.cursor.fetchone()
-        db.cursor.close()
-        db.conexao.close()
+        db.conectar()
+        query = '''SELECT * FROM Alunos WHERE matricula = %s'''
+        db.get_cursor().execute(query, (self.__matricula,))
+        aluno = db.get_cursor().fetchone()
+        db.get_cursor().close()
+        db.get_conexao().close()
         return aluno
 
     def atualizar_aluno(self, novo_nome=None, nova_data_nascimento=None, nova_matricula=None, novo_id_curso_fk=None):
         db = Database()
+        db.conectar()
         query = '''UPDATE Alunos 
                    SET nome = %s, data_nascimento = %s, matricula = %s, id_curso_fk = %s 
                    WHERE id_aluno = %s OR matricula = %s'''
-        db.cursor.execute(query, (
+        db.get_cursor().execute(query, (
             novo_nome or self.__nome, 
             nova_data_nascimento or self.__data_nascimento, 
             nova_matricula or self.__matricula, 
             novo_id_curso_fk or self.__id_curso_fk,
-            self.__id_aluno,
             self.__matricula
         ))
-        db.conexao.commit()
-        db.cursor.close()
-        db.conexao.close()
+        db.get_conexao().commit()
+        db.get_cursor().close()
+        db.get_conexao().close()
         return True
 
     def deletar_aluno(self):
         db = Database()
-        query = '''DELETE FROM Alunos WHERE id_aluno = %s OR matricula = %s'''
-        db.cursor.execute(query, (self.__id_aluno, self.__matricula))
-        db.conexao.commit()
-        db.cursor.close()
-        db.conexao.close()
+        db.conectar()
+        query = '''DELETE FROM Alunos WHERE matricula = %s'''
+        db.get_cursor().execute(query, (self.__id_aluno, self.__matricula))
+        db.get_conexao().commit()
+        db.get_cursor().close()
+        db.get_conexao().close()
         return True
+
+
---
+++ programa_sistema_academico/database.py
@@ -0,0 +1,37 @@
+from config import Config
+import mysql.connector
+from mysql.connector import Error
+
+class Database:
+    def __init__(self):
+        self.__host=Config.DB_HOST
+        self.__port=Config.DB_PORT
+        self.__user=Config.DB_USER
+        self.__password=Config.DB_PASSWORD
+        self.__database=Config.DB_DATABASE
+        self.__conexao=None
+        self.__cursor=None
+
+    
+    def conectar(self):
+        try:
+            self.__conexao = mysql.connector.connect(
+                host=self.__host,
+                port=self.__port,
+                user=self.__user,
+                password=self.__password,
+                database=self.__database
+            )
+            self.__cursor = self.__conexao.cursor()
+        except Error as err:
+            self.__conexao = None
+            self.__cursor = None
+            print(f'Error starting database:\n{err}') 
+
+    
+    def get_cursor(self):
+        return self.__cursor
+
+    def get_conexao(self):
+        return self.__conexao
+        
